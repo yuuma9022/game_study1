01189a559d4f4183f01334ed4996d669bca150bf
yuuma9022 <82853392+yuuma9022@users.noreply.github.com>
2025-04-01 12:01:45 +0900

    ５回目
    ５回目

---
+++ !!5_修了（文字の代わりに、キャラクター（動かない）を背景上に配置し、矢印キーによるキャラ移動操作の追加）/main.js
@@ -0,0 +1,171 @@
+"use strict";
+
+const CHRHEIGHT = 9;
+const CHRWIDTH = 8;
+const FONT = "48px monospace";
+const HEIGHT = 120;
+const WIDTH = 128;
+const MAP_HEIGHT = 120;
+const MAP_EIDTH = 32;
+const SMOOTH = 0;
+const TILECOLUMN = 4;
+const TILEROW = 4;
+const TILESIZE =8;
+
+let gScreen;
+let gFrame = 0;
+let gHeight;
+let gWidth;
+let gImgMap;
+let gImgPlayer;
+let gPlayerX = 10;
+let gPlayerY = 5;
+
+const gFileMap = "img/map.png";
+const gFiePlayer = "img/player.png";
+
+//マップを作成
+const gMap = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+];
+
+
+function DrawMain(){
+    const g = gScreen.getContext("2d");
+
+    //キャンバス内できれいな正方形表示にさせるため、20×20×8bitに設定
+    for(let y = 0 ;y < 20; y++){
+        for(let x = 0 ;x < 20; x++){
+            let px = gPlayerX + x;
+            let py = gPlayerY + y;
+            DrawTile( g, x * TILESIZE, y * TILESIZE, gMap[ py * MAP_EIDTH + px ]);
+        }
+    }
+
+    //キャラクター画像の切り取りと貼り付け(キャラ画像,xの切取始点,yの切取始点,xの切取大きさ,yの切取大きさ,xの貼付始点,yの貼付始点,xの貼付大きさ,yの貼付大きさ)
+    // WIDTH / 2, HEIGHT / 2　で背景画像の中央位置から右下に向かってキャラを張り付けるよう設定(キャラ画像8ビット分右下にずれる)
+    g.drawImage( gImgPlayer, CHRWIDTH, 0, CHRWIDTH, CHRHEIGHT,
+                 WIDTH / 2, HEIGHT / 2, CHRWIDTH, CHRHEIGHT);
+
+    // g.font = FONT;
+    // g.fillText("Hello world" + gFrame, gFrame /10, 64);
+
+}
+
+
+
+function DrawTile( g, x, y, idx){
+    const ix = ( idx % TILECOLUMN ) * TILESIZE;
+    const iy = Math.floor( idx / TILECOLUMN ) * TILESIZE;
+    g.drawImage( gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+} 
+
+
+//以下、追加した,画像取得、変数代入用のメソッド
+function LoadImage(){
+    //仮想変数を作成
+    gImgMap = new Image();
+    //作成した変数に代入元の画像を選択
+    gImgMap.src = gFileMap;
+    //仮想変数を作成
+    gImgPlayer = new Image();
+    //作成した変数に代入元の画像を選択
+    gImgPlayer.src = gFiePlayer;
+}
+
+
+function WmPaint(){
+    DrawMain();
+
+    const ca = document.getElementById("main");
+    const g  = ca.getContext("2d");
+
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight);
+}
+
+
+function WmSize(){
+    const ca = document.getElementById("main");
+    ca.width = window.innerWidth;
+    ca.height = window.innerHeight;
+    const g  = ca.getContext("2d");
+
+    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
+
+    gWidth = ca.width;
+    gHeight = ca.height;
+
+    if(gWidth / WIDTH < gHeight / HEIGHT){
+        gHeight = gWidth * WIDTH / HEIGHT;
+    }else{
+        gWidth = gHeight * WIDTH / HEIGHT;
+    }
+}
+
+
+
+function WmTimer(){
+    gFrame++;
+    WmPaint();
+}
+
+//以下、追加した、ボタン操作によるキャラ移動設定のメソッド
+//指定したボタン押下時に実行されるイベント（途中の何かしらの操作起動のメソッドにおいては、function(ev)にすることで、起動きっかけの詳細について記述することが可能になる）
+window.onkeydown = function(ev){
+    let c = ev.keyCode;
+    //矢印左キー
+    if(c == 37) gPlayerX--;
+    //矢印下キー
+    if(c == 38) gPlayerY--;
+    //矢印右キー
+    if(c == 39) gPlayerX++;
+    //矢印上キー
+    if(c == 40) gPlayerY++;
+}
+
+
+window.onload = function(){
+
+                    //最初に必要な画像をダウンロードするメソッドを実行
+                    LoadImage();
+
+                    gScreen = document.createElement("canvas");
+                    gScreen.width = WIDTH;
+                    gScreen.height = HEIGHT;
+
+                    WmSize();
+                    window.addEventListener("resize", function(){WmSize
+                    ()});
+                    setInterval(function(){WmTimer()}, 33);
+                }
