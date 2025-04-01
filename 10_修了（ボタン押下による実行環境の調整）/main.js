2295ef907da4e30db33bcefffe41d3ec0fefdc6a
yuuma9022 <82853392+yuuma9022@users.noreply.github.com>
2025-04-01 14:38:59 +0900

    １０回目
    １０回目

---
+++ 10_修了（ボタン押下による実行環境の調整）/main.js
@@ -0,0 +1,225 @@
+"use strict";
+
+const CHRHEIGHT = 9;
+const CHRWIDTH = 8;
+const FONT = "12px monospace";
+const FONTSTYLE = "#ffffff";//（白)
+const HEIGHT = 120;
+const WIDTH = 128;
+const MAP_HEIGHT = 32;
+const MAP_WIDTH = 32;
+const SCR_WIDTH = 8;
+const SCR_HEIGHT = 8;
+const START_X = 15;
+const START_Y = 17;
+const SMOOTH = 0;
+const TILECOLUMN = 4;
+const TILEROW = 4;
+const TILESIZE =8;
+const WNDSTYLE = "rgba( 0, 0, 0, 0.75 )";
+
+//キーボード標準0x100を使用するために変数を取得
+const gKey = new Uint8Array( 0x100 );
+
+let gScreen;
+let gFrame = 0;
+let gHeight;
+let gWidth;
+//移動量？？の追加
+let gMoveX = 0;
+let gMoveY = 0;
+let gImgMap;
+let gImgPlayer;
+//プレイヤー座標の調整
+let gPlayerX = START_X * TILESIZE + TILESIZE / 2;
+let gPlayerY = START_Y * TILESIZE + TILESIZE / 2;
+
+const gFileMap = "img/map.png";
+const gFiePlayer = "img/player.png";
+
+//マップを作成
+const gMap = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+];
+
+
+function DrawMain(){
+    const g = gScreen.getContext("2d");
+
+    let mx = Math.floor( gPlayerX / TILESIZE);
+    let my = Math.floor( gPlayerY / TILESIZE);
+
+    //背景初期位置貼り付け用ループ算術の内容変更
+    for(let dy = -SCR_HEIGHT ; dy <= SCR_HEIGHT; dy++){
+        let ty = my + dy;
+        let py = ( ty + MAP_HEIGHT ) % MAP_HEIGHT;
+        for(let dx = -SCR_WIDTH ;dx <= SCR_WIDTH; dx++){
+            let tx = mx + dx;
+            let px = ( tx + MAP_WIDTH ) % MAP_WIDTH;
+            DrawTile( g,
+                tx * TILESIZE + WIDTH / 2 - gPlayerX,
+                ty * TILESIZE + HEIGHT / 2 - gPlayerY,
+                gMap[ py * MAP_WIDTH + px ]);
+        }
+    }
+
+    g.fillStyle = "#ff0000";
+    g.fillRect(0, HEIGHT / 2 - 1, WIDTH, 2);
+    g.fillRect(WIDTH / 2 - 1, 0, 2, HEIGHT);
+
+    g.drawImage( gImgPlayer, CHRWIDTH, 0, CHRWIDTH, CHRHEIGHT,
+                 WIDTH / 2 - CHRWIDTH / 2, HEIGHT / 2 - CHRHEIGHT + TILESIZE / 2, CHRWIDTH, CHRHEIGHT);
+
+    g.fillStyle = WNDSTYLE;
+    g.fillRect( 20 , 103, 105, 15 );
+
+    g.font = FONT;
+    g.fillStyle = FONTSTYLE;
+    //(キャラクターのx座標とy座標とマップの種類を表示)
+    g.fillText( "x=" + gPlayerX + " y=" + gPlayerY + " m=" + gMap[ my * MAP_WIDTH + mx], 25, 115 );
+}
+
+
+
+function DrawTile( g, x, y, idx){
+    const ix = ( idx % TILECOLUMN ) * TILESIZE;
+    const iy = Math.floor( idx / TILECOLUMN ) * TILESIZE;
+    g.drawImage( gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+} 
+
+
+function LoadImage(){
+    gImgMap = new Image();
+    gImgMap.src = gFileMap;
+    gImgPlayer = new Image();
+    gImgPlayer.src = gFiePlayer;
+}
+
+
+//フィールド進行処理メソッド
+function TickField(){
+
+    //移動量x,yどちらかが０である場合の処理
+    if( gMoveX != 0 || gMoveY != 0){
+    }else if( gKey [ 37 ] ){
+        gMoveX = -TILESIZE;
+    }else if( gKey [ 38 ] ){
+        gMoveY = -TILESIZE;
+    }else if( gKey [ 39 ] ){
+        gMoveX = TILESIZE;
+    }else if( gKey [ 40 ] ){
+        gMoveY = TILESIZE;
+    };
+
+    //
+    gPlayerX += Math.sign( gMoveX );
+    gPlayerY += Math.sign( gMoveY );
+    gMoveX -= Math.sign( gMoveX );
+    gMoveY -= Math.sign( gMoveY );
+
+
+    gPlayerX += ( MAP_WIDTH * TILESIZE );
+    gPlayerX %= ( MAP_WIDTH * TILESIZE );
+    gPlayerY += ( MAP_HEIGHT * TILESIZE );
+    gPlayerY %= ( MAP_HEIGHT * TILESIZE );
+
+
+}
+
+
+function WmPaint(){
+    DrawMain();
+
+    const ca = document.getElementById("main");
+    const g  = ca.getContext("2d");
+
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight);
+}
+
+
+function WmSize(){
+    const ca = document.getElementById("main");
+    ca.width = window.innerWidth;
+    ca.height = window.innerHeight;
+    const g  = ca.getContext("2d");
+
+    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
+
+    gWidth = ca.width;
+    gHeight = ca.height;
+
+    if(gWidth / WIDTH < gHeight / HEIGHT){
+        gHeight = gWidth * WIDTH / HEIGHT;
+    }else{
+        gWidth = gHeight * WIDTH / HEIGHT;
+    }
+}
+
+
+
+function WmTimer(){
+    gFrame++;
+    //フィールド進行処理メソッドの実行
+    TickField();
+    WmPaint();
+}
+
+
+//指定したキーを押している間に実行されるメソッド
+window.onkeydown = function(ev){
+    let c = ev.keyCode;
+    //
+    gKey[ c ] = 1;
+}
+
+//指定したキーを離したときに実行されるメソッド
+window.onkeyup = function(ev){
+    //
+    gKey[ ev.keyCode ] = 0;
+}
+
+
+window.onload = function(){
+
+                    LoadImage();
+
+                    gScreen = document.createElement("canvas");
+                    gScreen.width = WIDTH;
+                    gScreen.height = HEIGHT;
+
+                    WmSize();
+                    window.addEventListener("resize", function(){WmSize
+                    ()});
+                    setInterval(function(){WmTimer()}, 33);
+                }
