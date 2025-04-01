ed72c2d9e189dc85f7e53216876077edcca7bb07
yuuma9022 <82853392+yuuma9022@users.noreply.github.com>
2025-04-01 14:39:25 +0900

    １１回目
    １１回目

---
+++ 11_未修了(読み込み理解はまだ未完成＿キャラクターの向き、足踏み要素、歩けない背景の選択を追加)/main.js
@@ -0,0 +1,235 @@
+"use strict";
+
+const CHRHEIGHT = 9;
+const CHRWIDTH = 8;
+const FONT = "12px monospace";
+const FONTSTYLE = "#ffffff";//（白)
+const HEIGHT = 120;
+const WIDTH = 128;
+const MAP_HEIGHT = 32;
+const MAP_WIDTH = 32;
+const SCR_WIDTH = 8;
+const SCR_HEIGHT = 8;
+const START_X = 15;
+const START_Y = 17;
+const SMOOTH = 0;
+const TILECOLUMN = 4;
+const TILEROW = 4;
+const TILESIZE =8;
+const WNDSTYLE = "rgba( 0, 0, 0, 0.75 )";
+
+const gKey = new Uint8Array( 0x100 );
+
+let gScreen;
+let gFrame = 0;
+//キャラクターの向き調整用変数を作成
+let gAngle = 0;
+let gHeight;
+let gWidth;
+let gMoveX = 0;
+let gMoveY = 0;
+let gImgMap;
+let gImgPlayer;
+let gPlayerX = START_X * TILESIZE + TILESIZE / 2;
+let gPlayerY = START_Y * TILESIZE + TILESIZE / 2;
+
+const gFileMap = "img/map.png";
+const gFiePlayer = "img/player.png";
+
+
+const gMap = [
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 3, 6, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 3, 3, 6, 6, 7, 7, 7, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 6, 3, 0, 0, 0, 3, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 6, 6, 6, 7, 7, 2, 2, 2, 7, 7, 2, 2, 2, 7, 7, 6, 3, 3, 3, 6, 6, 3, 6,13, 6, 0, 0, 0,
+0, 3, 3,10,11, 3, 3, 6, 7, 7, 2, 2, 2, 2, 2, 2, 1, 1, 7, 6, 6, 6, 6, 6, 3, 0, 6, 6, 6, 0, 0, 0,
+0, 0, 3, 3, 3, 0, 3, 3, 3, 7, 7, 2, 2, 2, 2, 7, 7, 1, 1, 6, 6, 6, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 7, 7, 7, 7, 2, 7, 6, 3, 1, 3, 6, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 7, 2, 7, 6, 3, 1, 3, 3, 6, 6, 3, 0, 0, 0, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 0, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 6, 7, 7, 7, 6, 3, 1, 1, 3, 3, 6, 3, 3, 0, 3,12, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 7, 7, 6, 3, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 6, 6, 6, 6, 3, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 6, 6, 3, 3, 3, 3, 1, 1, 3, 3, 3, 1, 1, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 5, 3, 3, 3, 6, 6, 6, 3, 3, 3, 1, 1, 1, 1, 1, 3, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 8, 9, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 3, 3, 1, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 3, 3, 3, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 3, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 6, 3, 6, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 6, 3, 6, 6, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 6, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,14, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 6, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 6, 7, 0, 0, 0, 0, 0, 0, 0, 0,
+7,15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 0, 0, 0, 0, 0,
+7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7,
+];
+
+
+function DrawMain(){
+    const g = gScreen.getContext("2d");
+
+    let mx = Math.floor( gPlayerX / TILESIZE);
+    let my = Math.floor( gPlayerY / TILESIZE);
+
+    for(let dy = -SCR_HEIGHT ; dy <= SCR_HEIGHT; dy++){
+        let ty = my + dy;
+        let py = ( ty + MAP_HEIGHT ) % MAP_HEIGHT;
+        for(let dx = -SCR_WIDTH ;dx <= SCR_WIDTH; dx++){
+            let tx = mx + dx;
+            let px = ( tx + MAP_WIDTH ) % MAP_WIDTH;
+            DrawTile( g,
+                tx * TILESIZE + WIDTH / 2 - gPlayerX,
+                ty * TILESIZE + HEIGHT / 2 - gPlayerY,
+                gMap[ py * MAP_WIDTH + px ]);
+        }
+    }
+
+    //プレイヤーの向き要素を加えて画像描写
+    //(足踏みをするように計算式を追加)
+    g.drawImage( gImgPlayer,
+        ( gFrame >> 4 & 1 ) * CHRWIDTH, gAngle * CHRHEIGHT, CHRWIDTH, CHRHEIGHT,
+        WIDTH / 2 - CHRWIDTH / 2, HEIGHT / 2 - CHRHEIGHT + TILESIZE / 2, CHRWIDTH, CHRHEIGHT);
+
+    g.fillStyle = WNDSTYLE;
+    g.fillRect( 20 , 103, 105, 15 );
+
+    g.font = FONT;
+    g.fillStyle = FONTSTYLE;
+
+    g.fillText( "x=" + gPlayerX + " y=" + gPlayerY + " m=" + gMap[ my * MAP_WIDTH + mx], 25, 115 );
+}
+
+
+
+function DrawTile( g, x, y, idx){
+    const ix = ( idx % TILECOLUMN ) * TILESIZE;
+    const iy = Math.floor( idx / TILECOLUMN ) * TILESIZE;
+    g.drawImage( gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+} 
+
+
+function LoadImage(){
+    gImgMap = new Image();
+    gImgMap.src = gFileMap;
+    gImgPlayer = new Image();
+    gImgPlayer.src = gFiePlayer;
+}
+
+
+//フィールド進行処理メソッド
+function TickField(){
+
+    if( gMoveX != 0 || gMoveY != 0){
+    }else if( gKey [ 37 ] ){
+        gMoveX = -TILESIZE;
+        gAngle = 1;
+    }else if( gKey [ 38 ] ){
+        gMoveY = -TILESIZE;
+        gAngle = 3;
+    }else if( gKey [ 39 ] ){
+        gMoveX = TILESIZE;
+        gAngle = 2;
+    }else if( gKey [ 40 ] ){
+        gMoveY = TILESIZE;
+        gAngle = 0;
+    };
+
+    //移動後の背景が何であるかを取得
+    let mx = Math.floor( ( gPlayerX + gMoveX ) / TILESIZE );
+    let my = Math.floor( ( gPlayerY + gMoveY ) / TILESIZE );
+    mx += MAP_WIDTH;
+    mx %= MAP_WIDTH;
+    my += MAP_HEIGHT;
+    my %= MAP_HEIGHT;
+    let m = gMap[ my * MAP_WIDTH + mx ];
+    //以下で、マップ番号が3未満の場合は移動できないように設定
+     if( m < 3 ){
+        gMoveX = 0;
+        gMoveY = 0;
+     }
+
+
+    gPlayerX += Math.sign( gMoveX );
+    gPlayerY += Math.sign( gMoveY );
+    gMoveX -= Math.sign( gMoveX );
+    gMoveY -= Math.sign( gMoveY );
+
+
+    gPlayerX += ( MAP_WIDTH * TILESIZE );
+    gPlayerX %= ( MAP_WIDTH * TILESIZE );
+    gPlayerY += ( MAP_HEIGHT * TILESIZE );
+    gPlayerY %= ( MAP_HEIGHT * TILESIZE );
+
+
+}
+
+
+function WmPaint(){
+    DrawMain();
+
+    const ca = document.getElementById("main");
+    const g  = ca.getContext("2d");
+
+    g.drawImage(gScreen, 0, 0, gScreen.width, gScreen.height, 0, 0, gWidth, gHeight);
+}
+
+
+function WmSize(){
+    const ca = document.getElementById("main");
+    ca.width = window.innerWidth;
+    ca.height = window.innerHeight;
+    const g  = ca.getContext("2d");
+
+    g.imageSmoothingEnabled = g.msImageSmoothingEnabled = SMOOTH;
+
+    gWidth = ca.width;
+    gHeight = ca.height;
+
+    if(gWidth / WIDTH < gHeight / HEIGHT){
+        gHeight = gWidth * WIDTH / HEIGHT;
+    }else{
+        gWidth = gHeight * WIDTH / HEIGHT;
+    }
+}
+
+
+
+function WmTimer(){
+    gFrame++;
+    TickField();
+    WmPaint();
+}
+
+
+window.onkeydown = function(ev){
+    let c = ev.keyCode;
+    gKey[ c ] = 1;
+}
+
+
+window.onkeyup = function(ev){
+    gKey[ ev.keyCode ] = 0;
+}
+
+
+window.onload = function(){
+
+                    LoadImage();
+
+                    gScreen = document.createElement("canvas");
+                    gScreen.width = WIDTH;
+                    gScreen.height = HEIGHT;
+
+                    WmSize();
+                    window.addEventListener("resize", function(){WmSize
+                    ()});
+                    setInterval(function(){WmTimer()}, 33);
+                }
